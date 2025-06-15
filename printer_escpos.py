3efa88c0ec3b4b1d59a49c7a2f4d1c1bb58cd6f3
Tristan Rodriguez <161824279+trodlop@users.noreply.github.com>
2025-06-15 15:12:32 +0100

    Add files via upload

--- printer_escpos.py
+++ printer_escpos.py
@@ -1,8 +1,9 @@
 from escpos.printer import Serial
 from PIL import Image
+import os
 
 def image_resize(path, target_width):
-    image = Image.open("printer/" + path)
+    image = Image.open(path)
     scaling_factor = (target_width / float(image.size[0]))
     target_height = int((float(image.size[1]) * float(scaling_factor)))
     resized_image = image.resize((target_width, target_height), Image.LANCZOS)
@@ -16,18 +17,79 @@ p = Serial(devfile="COM1",
            parity="N",
            stopbits=1,
            timeout=1.00)
-print("Intructions on use:")
-print("1) Move the desired image into a folder named 'printer' in the same directory as the script")
-print("2) Run the script")
-print("3) When prompted, type in the file name and press enter")
-print("\nNote: that the file extension is also needed (for example, image1.jpg)\nImages are automatically resized to be 500px wide")
 
-file_name = str(input("File name: "))
-resized_file_path = image_resize(file_name, 500)
+# Print image:
+def print_image(file_name, path):
+    
+    resized_file_path = image_resize(path, 500)
+    
+    print("Printing {}...".format(file_name))
+    p.image(resized_file_path)
+    p.cut()
+        
 
-print("Printing {}...".format(file_name))
+# Print txt file:
+def print_txt(file_name, path):
 
-p.image(resized_file_path)
-p.cut()
+    with open(path, "r", encoding = "utf-8") as file:
+        text = file.read()
 
-print("Print complete...")
+    print("Printing {}...".format(file_name))
+    p.text(text)
+    p.cut()
+
+    
+# Select file:
+def select_file(file_type):
+    
+    valid_file_name = False
+    
+    while valid_file_name == False:
+        file_name = str(input("File name: "))
+        full_path = "printer/" + file_name
+
+        if os.path.exists(full_path):
+            print(file_type)
+            if file_type == "image":
+                print_image(file_name, full_path)
+            elif file_type == "text":
+                print_txt(file_name, full_path)
+                
+            valid_file_name = True
+                
+        else:
+            print(f"Entered file does not exist. You entered '{file_name}'")
+            valid_file_name == False
+
+
+# Select file type and print
+def print_file():
+
+    valid_file_type = False
+
+    while valid_file_type == False:
+
+        print("Please select file type:\n- Image\n- Text")
+        file_type = str(input(""))
+        
+        lower_file_type = file_type.lower()
+
+        if lower_file_type == "image":
+            select_file(lower_file_type)
+            valid_file_type = True
+        elif lower_file_type == "text":
+            select_file(lower_file_type)
+            valid_file_type = True
+        else:
+            print(f"Entered file type is incorrect. You typed in '{file_type}'") 
+            valid_file_type = False
+
+
+if __name__ == "__main__":
+    print("Intructions on use:")
+    print("Simply type in the file name into the prompt and press enter")
+    print("\nNote: that the file extension is also needed (for example, image1.jpeg)\nMaximum width for images is 500px")
+
+    print_file()
+
+    print("Print complete...")
